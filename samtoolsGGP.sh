36b44dde0f6cb7dd4e1a033f11a079e2b0dbbd73
todd_pihl <todd.pihl@gmail.com>
2016-12-23 20:17:05 +0000

    Adding script to work directly with GGP

--- samtoolsGGP.sh
+++ samtoolsGGP.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#Heavily modified version of startFastqc.sh 
-#https://github.com/isb-cgc/ISB-CGC-pipelines/blob/master/lib/examples/launch_scripts/startFastqc.sh
+# Heavily modified version of startFastqc.sh that uses Google Genomics Piplein instead of ISB-CGC Pipeline
+# https://github.com/isb-cgc/ISB-CGC-pipelines/blob/master/lib/examples/launch_scripts/startFastqc.sh
 # Last modified
 
-usage() { echo "Usage: $0 [-i <input file>] [-o <output directory>] [-l <log directory>] [-j <job name>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-i <input file>] [-o <output directory>] [-l <log directory>] [-p <pipeline file>]" 1>&2; exit 1; }
 
 JOBNAME="samtools-index"
-while getopts "i:o:l:j:" args; do
+while getopts "i:o:l:p:" args; do
   case "${args}" in
     i)
 	  INPUTFILE=${OPTARG}
@@ -18,16 +18,16 @@ while getopts "i:o:l:j:" args; do
 	l)
 	  LOGDIR=${OPTARG}
 	  ;;
-	j)
-	  JOBNAME=${OPTARG}
+        p)
+          PIPELINEFILE=${OPTARG}
+          ;;
+        *)
+	  usage
 	  ;;
-	 *)
-	   usage
-	   ;;
   esac
 done
 
-if [ -z "${INPUTFILE}" ] || [ -z "${OUTDIR}" ] || [ -z "${LOGDIR}" ]; then
+if [ -z "${INPUTFILE}" ] || [ -z "${OUTDIR}" ] || [ -z "${LOGDIR}" ] || [ -z "${PIPELINEFILE}" ]; then
   usage
 fi
 
@@ -40,6 +40,17 @@ fi
 if  [[ $LOGDIR != "gs://"* ]]; then
   echo "Log directory should be a Google bucket"
   exit 1
+fi
+
+#Test to make sure the pipelinefile exists
+if [ ! -e $PIPELINEFILE ]; then
+  echo "${PIPELINEFILE} does not exist.  Please specific an existing pipeline file"
+  usage
+fi
+ 
+#Put a / on the end of the output directory if it isn't there already
+if [[ "${OUTIDIR:-1}" != "/" ]]; then
+  OUTDIR=$OUTDIR/
 fi
 
 #If the user installed ISB-CGC tools correctly, calculateDiskSize should be in /usr/bin
@@ -56,6 +67,10 @@ while read bamFile; do
     bamFileName=$(echo $bamFile | rev | cut -d '/' -f 1 | rev)
     echo "Processing ${bamFileName}"
 
+   #Create a name for the bai file
+    bamFileName=$(echo $bamFile | rev | cut -d '/' -f 1 | rev)
+    baiFileName="${bamFileName}.bai"
+
     # Use the "calculateDiskSize" utility script to calculate the size of the disk if it is known
 	if [ -e $CALCSCRIPT ]; then
           diskSize=$($CALCSCRIPT --inputFile $bamFile --roundToNearestGbInterval 100)
@@ -65,16 +80,12 @@ while read bamFile; do
 	fi
 
     # Submit a task to the Google Genomics Pipelines API for the given BAM file
-	isb-cgc-pipelines submit --pipelineName $JOBNAME \
-		--inputs "${bamFile}:${bamFileName}" \
-		--outputs "*bai:${OUTDIR}" \
-		--cmd "samtools index $bamFileName" \
-		--imageName nareshr/samtoolsindex \
-		--cores 1 --mem 2 \
-		--diskSize $diskSize \
-                --logsPath $LOGDIR \
-		--preemptible
-
+	gcloud alpha genomics pipelines run \
+                --pipeline-file $PIPELINEFILE \
+                --logging $LOGDIR \
+                --inputs INPUT_FILE=$bamFile \
+                --outputs OUTPUT_FILE=${OUTDIR}${baiFileName} \
+                --disk-size datadisk:$diskSize
     #Add a wait time to avoid overwhelming the scheduler
     sleep 5
 
